5ee1792a41e1b4d849a336125ab45950250d1db8
hafizurrahman42 <96075390+hafizurrahman42@users.noreply.github.com>
2022-03-19 18:48:19 +0000

    18:48 committed

--- src/internationalstdasst/InternationalStdAsst.java
+++ src/internationalstdasst/InternationalStdAsst.java
@@ -7,6 +7,7 @@ public class InternationalStdAsst {
  
     public static void main(String[] args) {
         System.out.println("Hello This is Hafiz");
+        System.out.println("This is another print");
     }
     
 }
