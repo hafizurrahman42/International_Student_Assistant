4165127a9f2985aef7dbdf21e94c608774628332
hafizurrahman42 <96075390+hafizurrahman42@users.noreply.github.com>
2022-03-19 13:04:54 +0000

    18:41 committed

---
+++ src/internationalstdasst/InternationalStdAsst.java
@@ -0,0 +1,12 @@
+
+package internationalstdasst;
+
+
+public class InternationalStdAsst {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello This is Hafiz");
+    }
+    
+}
