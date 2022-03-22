50e6995c857ef5023139ab878120a6208053da5b
kazi ashiqur rahman <rahman.kaziashiqur.info@gmail.com>
2022-03-22 16:18:16 +0000

    ashiq commit

--- src/internationalstdasst/InternationalStdAsst.java
+++ src/internationalstdasst/InternationalStdAsst.java
@@ -9,6 +9,7 @@ public class InternationalStdAsst {
         System.out.println("Hello This is Hafiz");
         System.out.println("This is another print");
         System.out.println("This is another push");
+        System.out.println("this is ashiq");
     }
     
 }
